3070177447a2c033e7dcc10b54654ec97d81d8c4
edicodes1 <ca1510797@gmail.com>
2023-11-28 21:54:41 +0200

    4455

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-// Replace with your Firebase config
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyAmUhBfF6x8YxljEcpruBxx1bMKVn8FkSU",
+  authDomain: "edicodes.firebaseapp.com",
+  projectId: "edicodes",
+  storageBucket: "edicodes.appspot.com",
+  messagingSenderId: "643619931425",
+  appId: "1:643619931425:web:b2a5eacd46f40f5680aa60",
+  measurementId: "G-62Q03G63BG"
 };
 
 // Initialize Firebase
@@ -25,6 +26,8 @@ loginForm.addEventListener('submit', function (e) {
   auth.signInWithEmailAndPassword(email, password)
       .then(() => {
           console.log('Login successful');
+          // Redirect to another page after successful login
+          window.location.href = 'dashboard.html';
       })
       .catch(error => {
           console.error(error.message);
@@ -42,6 +45,8 @@ signupForm.addEventListener('submit', function (e) {
   auth.createUserWithEmailAndPassword(email, password)
       .then(() => {
           console.log('Signup successful');
+          // Redirect to another page after successful signup
+          window.location.href = 'en.html';
       })
       .catch(error => {
           console.error(error.message);
