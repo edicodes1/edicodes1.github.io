155fdcce68c04d2cc02c6e51eacd2d155cd4d57c
edicodes1 <ca1510797@gmail.com>
2023-11-29 11:29:31 +0200

    8787

--- script.js
+++ script.js
@@ -15,27 +15,39 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 
 function login() {
+    console.log("Login function called");
+    
     const email = document.getElementById("loginEmail").value;
     const password = document.getElementById("loginPassword").value;
 
+    console.log("Attempting login with email:", email);
+    
     auth.signInWithEmailAndPassword(email, password)
         .then((userCredential) => {
+            console.log("Login successful!", userCredential);
             alert("Login successful!");
         })
         .catch((error) => {
+            console.error("Login error:", error);
             alert(error.message);
         });
 }
 
 function signup() {
+    console.log("Signup function called");
+
     const email = document.getElementById("signupEmail").value;
     const password = document.getElementById("signupPassword").value;
 
+    console.log("Attempting signup with email:", email);
+
     auth.createUserWithEmailAndPassword(email, password)
         .then((userCredential) => {
+            console.log("Signup successful!", userCredential);
             alert("Sign up successful!");
         })
         .catch((error) => {
+            console.error("Signup error:", error);
             alert(error.message);
         });
 }
